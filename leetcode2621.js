7883a95219d22e59383514040c66fd072139f974
harsh vardhan singh <124882370+xitLiQuidity@users.noreply.github.com>
2026-09-13 16:55:00 +0530

    Add sleep function with promise support
    Implement a sleep function that delays execution for a specified number of milliseconds, returning a promise. Includes error handling for invalid arguments.

---
+++ leetcode2621.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+  return new Promise((delayresolve, reject) => {
+    if (typeof millis !== 'number' || isNaN(millis)) {
+      reject(new Error('Invalid argument. Expected a number.'));
+    } else {
+      setTimeout(delayresolve, millis);
+    }
+  });
+}
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
